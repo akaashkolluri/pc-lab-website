ebdc130f492acd0677f09170f5f389efa98c3e7f
Akaash Kolluri <arkolluri@gmail.com>
2021-11-28 02:03:54 -0600

    updated homepage styling

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,29 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-
-      <main className={styles.main}>
+      <div className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to my{" "}
+          <div>
+            {" "}
+            <span> Physics C Labs</span>{" "}
+          </div>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
+        <h2 className={styles.subtitle}> Take a look around.</h2>
+        <h2 className={styles.subtitle2}> Stay a little while.</h2>
+        <img
+          src="/home1.png"
+          alt="Mr. Shah teaching us about energy."
+          className={styles.image1}
+        />
+        <p style={{ flex: 1 }} />
+      </div>
       <Footer />
     </div>
   );

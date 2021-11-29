c18e83db3920b24729f4218790bc06dcf7eb7932
Akaash Kolluri <arkolluri@gmail.com>
2021-11-29 11:47:25 -0600

    delted excess styling code + fixed pendula pdf

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import "../styles/globals.css";
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      {" "}
+      <title>Shah</title>
+      <Component {...pageProps} />{" "}
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/labs/1.js
+++ pages/labs/1.js
@@ -14,8 +14,19 @@ export default function Lab1() {
         <div className={styles.col}>
           <h2>Summary</h2>
           <h3>
-            In this lab, we attempt In this lab, we attemptIn this lab, we
-            attemptIn this lab, we attemptIn this lab, we attempt
+            In this lab, the value of g was experimentally determined. Three
+            different tests were conducted: first, a car was rolled down a ramp,
+            second, a tennis ball was dropped, and third a pendulum was put into
+            motion. In the first test, the car’s initial height, final height,
+            mass, and ramp length were kept constant, time was measured, and
+            gravity was calculated. In the second test, the tennis balls' mass
+            and drop height were kept constant, the time to drop was measured,
+            and gravity was calculated. In the third test, the pendulum string
+            length, the pendulum mass, and the pendulum start angle were kept
+            constant, the period time was measured, and gravity was calculated.
+            Gravity was found to be 10.987 m/s^2, 9.157 m/s^2, and 10.203 m/s^2
+            in the respective experiment. This result was useful as it confirmed
+            the value of gravity normally used in experiments.
           </h3>
         </div>
 
